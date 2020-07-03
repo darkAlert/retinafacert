c31edaaef15952e4f351358b19c6fb53ae7d8920
darkAlert <darkangel-nwo@yandex.ru>
2020-07-03 15:54:34 +0300

    init

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name='retinaface',
+    version='1.0.0',
+    description='RetinaFace-RT: Real-time Single-stage Dense Face Localisation in the Wild. ',
+    author='biubug6,darkAlert',
+    packages=setuptools.find_packages()
+)
